45b7eb82b45d52abc551a90eb04d22cf790bd50e
FrenchTastic <romainpardon@gmail.com>
2013-08-04 23:15:33 +0200

    Rajout de la route
    Rajout de la route API pour récupérer les données de test

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+﻿var data = [
+    {
+      "title": "Lorem ipsum",
+      "text": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+    },
+    {
+      "title": "Sed egestas",
+      "text": "Sed egestas, ante et vulputate volutpat, eros pede semper est, vitae luctus metus libero eu augue. Morbi purus libero, faucibus adipiscing, commodo quis, gravida id, est. Sed lectus."
+    }
+  ];
+
+exports.get = function (req, res) {
+  res.json(data);
+};
