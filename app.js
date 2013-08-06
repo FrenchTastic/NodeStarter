9be148c42054392ce1d46f3a5b8f65950cce8fd6
FrenchTastic <romainpardon@gmail.com>
2013-08-07 01:13:33 +0200

    Modifs directive

--- app.js
+++ app.js
@@ -3,48 +3,46 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-	, api = require('./routes/api')
-	, passport = require("passport")
-  , http = require('http')
-	, GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
-  , path = require('path');
+    , routes = require('./routes')
+    , user = require('./routes/user')
+    , api = require('./routes/api')
+    , passport = require("passport")
+    , http = require('http')
+    , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+    , path = require('path');
 
 var app = express();
-var GOOGLE_CLIENT_ID = "YOURCLIENID";
+var GOOGLE_CLIENT_ID = "YOURCLIENTID";
 var GOOGLE_CLIENT_SECRET ="YOURCLIENTSECRET";
 
-passport.serializeUser(function(user, done) {
-  console.log("Bernard " + user)
-  done(null, user);
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
 });
 
-passport.deserializeUser(function(obj, done) {
-  done(null, obj);
+passport.serializeUser(function(user, done) {
+    done(null, user);
 });
 
 passport.use(new GoogleStrategy({
-    //returnURL: process.env.IP + '/auth/google/return',
-    //realm: process.env.IP
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://pardon.azurewebsites.net/auth/google/callback",
-    scope: ['openid', 'email', 'profile'] 
-  },
-  function(accessToken, refreshToken, profile, done) {
-    // asynchronous verification, for effect...
-		console.log('Coucou Google');
-    process.nextTick(function () {
-      
-      // To keep the example simple, the user's Google profile is returned to
-      // represent the logged-in user.  In a typical application, you would want
-      // to associate the Google account with a user record in your database,
-      done(null, profile, { message:  'cest la merde' });
-      // and return that user instead.
-    });
-  }
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "/auth/google/callback",
+        scope: ['openid', 'email', 'profile']
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // asynchronous verification, for effect...
+        console.log('Coucou Google');
+        process.nextTick(function () {
+
+            // To keep the example simple, the user's Google profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the Google account with a user record in your database,
+            done(null, profile, { message:  'cest la merde' });
+            // and return that user instead.
+        });
+    }
 ));
+
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -65,7 +63,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+    app.use(express.errorHandler());
 }
 
 app.get('/', routes.index);
@@ -76,17 +74,17 @@ app.get('/api/someJson', api.get);
 app.get('/connected', routes.connected);
 
 app.get('/auth/google',
-  passport.authenticate('google'),
-  function(req, res){
-});
+    passport.authenticate('google'),
+    function(req, res){
+    });
 
 
 app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/partials/test' }),
-function(req, res) {
-	// Successful authentication, redirect home.
-	res.redirect('/connected');
-});
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/connected');
+    });
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
--- public/javascripts/directives.js
+++ public/javascripts/directives.js
@@ -1,13 +1,24 @@
-﻿myModule.directive('editTweet', function($location) {
+﻿myModule.directive('editTweet', function() {
   return{
 		restrict: 'A',
-		template: '<span ng-hide="editMode" ng-click="edit()" class="glyphicon glyphicon-pencil pull-right" style="margin-top:10px;"></span>' +
-		'<input type="text" ng-show="editMode" class="pull-right" style="margin-top:10px;"></span>',
+		template: '<span data-ng-hide="editMode" data-ng-click="edit()" class="glyphicon glyphicon-pencil pull-right" style="margin-top:10px;"></span>' +
+		'<input type="text" data-ng-show="editMode" class="pull-right" style="margin-top:10px;"></span>',
 		link: function(scope, element, attrs){
 			scope.editMode = false;
 			scope.edit = function() {
 				scope.editMode = true;
 			};
+			element.find('input').blur(function(){
+				scope.$apply(function(){
+					scope.editMode = false;
+				});
+			});
+			scope.$watch('editMode', function() {
+				if(scope.editMode)
+				{
+					element.find('input').focus();
+				}
+			});
 		}
 	};
 });
