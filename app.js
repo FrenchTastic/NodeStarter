dfb567435c5ff6ca6eeb29812685f852e9a10cf7
OpenShift guest <romainpardon@gmail.com>
2013-08-07 10:22:14 -0400

    Ajout app.js Node

---
+++ app.js
@@ -0,0 +1,87 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+    , routes = require('./routes')
+    , user = require('./routes/user')
+    , api = require('./routes/api')
+    , passport = require("passport")
+    , http = require('http')
+    , GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+    , path = require('path');
+
+var app = express();
+var GOOGLE_CLIENT_ID = "YOURCLIENTID";
+var GOOGLE_CLIENT_SECRET ="YOURSECRET";
+
+passport.deserializeUser(function(obj, done) {
+    done(null, obj);
+});
+
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+        clientID: GOOGLE_CLIENT_ID,
+        clientSecret: GOOGLE_CLIENT_SECRET,
+        callbackURL: "http://YOURURL/auth/google/callback",
+        scope: ['openid', 'email', 'profile']
+    },
+    function(accessToken, refreshToken, profile, done) {
+        // asynchronous verification, for effect...
+        console.log('Coucou Google');
+        process.nextTick(function () {
+
+            // To keep the example simple, the user's Google profile is returned to
+            // represent the logged-in user.  In a typical application, you would want
+            // to associate the Google account with a user record in your database,
+            done(null, profile, { message:  'cest la merde' });
+            // and return that user instead.
+        });
+    }
+));
+
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.use(express.cookieParser('keyboard cat'));
+app.use(express.session());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+// development only
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+app.get('/', routes.index);
+app.get('/partials/:name', routes.partials);
+app.get('/users', user.list);
+app.get('/api/someJson', api.get);
+app.get('/connected', routes.connected);
+
+app.get('/auth/google',passport.authenticate('google'));
+app.post('/api/tweet',api.postTweet);
+
+
+app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/partials/test' }),
+    function(req, res) {
+        // Successful authentication, redirect home.
+        res.redirect('/connected');
+    });
+
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
